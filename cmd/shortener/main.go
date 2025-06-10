4f38d59e287cee3d48ee9db3c691c5da7d484450
Damei Enik <enikdamei55gmail.com>
2025-06-10 16:34:35 -0700

    илья посмотри код

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,15 +1,36 @@
 package main
 
 import (
+	"crypto/sha1"
 	"fmt"
+	"io"
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "URL Shortener Service")
-	})
+var store = map[string]string{}
 
-	fmt.Println("Starting server at :8080")
-	http.ListenAndServe(":8080", nil)
+func handlers(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		v, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Failed to read request body", http.StatusBadRequest)
+			return
+		}
+		orig := string(v)
+		sum := sha1.Sum([]byte(orig))
+		id := fmt.Sprintf("%x", sum)[:8]
+		store[id] = orig
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte("http://localhost:8080/" + id))
+		return
+	}
+}
+
+func main() {
+	http.HandleFunc("/", handlers)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
 }
